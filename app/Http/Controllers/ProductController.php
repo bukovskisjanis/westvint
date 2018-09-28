651707b86c705df676226d87a7fd49b413dfcb1d
Ronalds Sovas <sovas@greenbox.lv>
2018-09-28 11:53:47 +0300

    only authorised users can open admin stuff

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,19 @@ use Auth;
 
 class ProductController extends Controller
 {
+
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    
     /**
      * Display a listing of the resource.
      *
