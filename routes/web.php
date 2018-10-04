bc7334eec966df614c62002b6151306abf714128
Ronalds Sovas <sovas@greenbox.lv>
2018-10-04 09:31:54 +0300

    start of invoice

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,12 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+
 Route::resource('darbinieki','DarbiniekiController');
 
 Route::resource('client','ClientController');
 
 Route::resource('product','ProductController');
+
+Route::get('/invoice', 'InvoiceController@generator')->name('generator');
